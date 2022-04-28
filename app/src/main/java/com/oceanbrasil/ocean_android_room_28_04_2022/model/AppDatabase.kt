164d3d407ba7ef26afc555f416b2ebe1c13c63ce
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-28 10:24:23 -0300

    Implementação do `AppDatabase`

--- app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/model/AppDatabase.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/model/AppDatabase.kt
@@ -1,4 +1,21 @@
 package com.oceanbrasil.ocean_android_room_28_04_2022.model
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [PokemonEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun pokemonDao(): PokemonDao
+
+    companion object {
+        fun getDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "app_database"
+            ).build()
+        }
+    }
 }
