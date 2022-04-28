84c2011f390b8604e6804a096a6fc7600f6f6267
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-28 11:44:40 -0300

    Formulário de criação de um pokémon

--- app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/view/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.oceanbrasil.ocean_android_room_28_04_2022.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.oceanbrasil.ocean_android_room_28_04_2022.R
@@ -31,6 +32,19 @@ class MainActivity : AppCompatActivity() {
         pokemonViewModel.create(PokemonEntity(null, "Charmander", "https://image"))
         pokemonViewModel.create(PokemonEntity(null, "Bulbasaur", "https://image"))
         pokemonViewModel.create(PokemonEntity(null, "Squirtle", "https://image"))
+
+        // Criação de pokémon
+        val etPokemon = findViewById<EditText>(R.id.etPokemon)
+        val btAdicionar = findViewById<Button>(R.id.btAdicionar)
+
+        btAdicionar.setOnClickListener {
+            val nomePokemon = etPokemon.text.toString()
+            val novoPokemon = PokemonEntity(null, nomePokemon, "https://image")
+            pokemonViewModel.create(novoPokemon)
+
+            // Limpar o EditText após adicionar
+            etPokemon.setText("")
+        }
 
         // Botão para limpar DB
         val btLimparDB = findViewById<Button>(R.id.btLimparDB)
