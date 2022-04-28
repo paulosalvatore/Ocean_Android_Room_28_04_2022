e321bc5b9ea83da0e58a4820f01cc039c611b931
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-28 10:07:33 -0300

    Criação da entidade `PokemonEntity`

--- app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/model/PokemonEntity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/model/PokemonEntity.kt
@@ -1,5 +1,14 @@
 package com.oceanbrasil.ocean_android_room_28_04_2022.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class PokemonEntity(
-    val name: String
+    @PrimaryKey
+    val id: Long?,
+
+    val name: String,
+
+    val imageUrl: String
 )
