a24dd7d018c7e0631afb51cfb1b1b6e5f42c82e5
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-28 10:13:38 -0300

    Implementação do `PokemonDao`

--- app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/model/PokemonDao.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/model/PokemonDao.kt
@@ -1,4 +1,15 @@
 package com.oceanbrasil.ocean_android_room_28_04_2022.model
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface PokemonDao {
+    @Query("SELECT * FROM PokemonEntity")
+    fun findAll(): LiveData<List<PokemonEntity>>
+
+    @Insert
+    fun create(pokemon: PokemonEntity)
 }
