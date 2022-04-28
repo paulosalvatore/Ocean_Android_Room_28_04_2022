e1143b2ca7e02559702043fff15b3167d7de4617
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-28 10:31:34 -0300

    Implementação do `PokemonViewModel`

--- app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_28_04_2022/viewmodel/PokemonViewModel.kt
@@ -1,4 +1,28 @@
 package com.oceanbrasil.ocean_android_room_28_04_2022.viewmodel
 
-class PokemonViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.oceanbrasil.ocean_android_room_28_04_2022.model.AppDatabase
+import com.oceanbrasil.ocean_android_room_28_04_2022.model.PokemonDao
+import com.oceanbrasil.ocean_android_room_28_04_2022.model.PokemonEntity
+
+class PokemonViewModel(application: Application) : AndroidViewModel(application) {
+    val pokemon: LiveData<List<PokemonEntity>>
+
+    private val pokemonDao: PokemonDao
+
+    init {
+        val db = AppDatabase.getDatabase(application)
+
+        pokemonDao = db.pokemonDao()
+
+        pokemon = pokemonDao.findAll()
+    }
+
+    fun create(pokemon: PokemonEntity) {
+        Thread {
+            pokemonDao.create(pokemon)
+        }.start()
+    }
 }
